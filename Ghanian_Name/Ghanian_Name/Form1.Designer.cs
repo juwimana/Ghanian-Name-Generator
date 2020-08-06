521b0d8e0416f0987e64d2da3d48f393d548dd7b
Jean Marie Uwimana <46766488+juwimana@users.noreply.github.com>
2020-08-06 16:38:39 -0700

    Add files via upload

---
+++ Ghanian_Name/Ghanian_Name/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Ghanian_Name
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox_day = new System.Windows.Forms.ComboBox();
+            this.comboBox_gender = new System.Windows.Forms.ComboBox();
+            this.label_name = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(638, 222);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(296, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "My Ghanian name is";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(205, 473);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(294, 57);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Find my name";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox_day
+            // 
+            this.comboBox_day.FormattingEnabled = true;
+            this.comboBox_day.Items.AddRange(new object[] {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"});
+            this.comboBox_day.Location = new System.Drawing.Point(205, 214);
+            this.comboBox_day.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox_day.Name = "comboBox_day";
+            this.comboBox_day.Size = new System.Drawing.Size(293, 45);
+            this.comboBox_day.TabIndex = 5;
+            this.comboBox_day.SelectedIndexChanged += new System.EventHandler(this.comboBox_day_SelectedIndexChanged_1);
+            // 
+            // comboBox_gender
+            // 
+            this.comboBox_gender.FormattingEnabled = true;
+            this.comboBox_gender.Items.AddRange(new object[] {
+            "Female",
+            "Male"});
+            this.comboBox_gender.Location = new System.Drawing.Point(205, 351);
+            this.comboBox_gender.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox_gender.Name = "comboBox_gender";
+            this.comboBox_gender.Size = new System.Drawing.Size(293, 45);
+            this.comboBox_gender.TabIndex = 6;
+            this.comboBox_gender.SelectedIndexChanged += new System.EventHandler(this.comboBox_gender_SelectedIndexChanged_1);
+            // 
+            // label_name
+            // 
+            this.label_name.AutoSize = true;
+            this.label_name.Font = new System.Drawing.Font("Times New Roman", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_name.Location = new System.Drawing.Point(634, 297);
+            this.label_name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_name.Name = "label_name";
+            this.label_name.Size = new System.Drawing.Size(0, 55);
+            this.label_name.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 161);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 39);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Day of the Week";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(210, 297);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 39);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Gender";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 612);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_name);
+            this.Controls.Add(this.comboBox_gender);
+            this.Controls.Add(this.comboBox_day);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox_day;
+        private System.Windows.Forms.ComboBox comboBox_gender;
+        private System.Windows.Forms.Label label_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Ghanian_Name/Ghanian_Name/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ghanian_Name
+{
+    public partial class Form1 : Form
+    {
+        string[] male_name = { "Kojo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame", "Kwesi" };
+        string[] female_name = { "Adjoa", "Abena", "Akua", "Yaa", "Afua", "Ama", "Akosua" };
+        string day = "";
+        string gender = "";
+        public Form1()
+        {
+            InitializeComponent();
+            //Change title of form
+            this.Text = "My Ghanian Name";
+            
+            // Changes the border to Fixed3D.
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+        }
+         public enum FlatStyle
+        {
+            Popup,
+        }
+        private void comboBox_day_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+           day = comboBox_day.SelectedItem.ToString();
+        }
+
+        private void comboBox_gender_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            gender = comboBox_gender.SelectedItem.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (gender == "Female")
+            {
+                if (day == "Monday")
+                {
+                    label_name.Text = female_name[0];
+                }
+                else if (day == "Tuesday")
+                {
+                    label_name.Text = female_name[1];
+                }
+                else if (day == "Wednesday")
+                {
+                    label_name.Text = female_name[2];
+                }
+                else if (day == "Thursday")
+                {
+                    label_name.Text = female_name[3];
+                }
+                else if (day == "Friday")
+                {
+                    label_name.Text = female_name[4];
+                }
+                else if (day == "Saturday")
+                {
+                    label_name.Text = female_name[5];
+                }
+                else if (day == "Sunday")
+                {
+                    label_name.Text = female_name[6];
+                }
+            }
+            else if (gender == "Male")
+            {
+                if (day == "Monday")
+                {
+                    label_name.Text = male_name[0];
+                }
+                else if (day == "Tuesday")
+                {
+                    label_name.Text = male_name[1];
+                }
+                else if (day == "Wednesday")
+                {
+                    label_name.Text = male_name[2];
+                }
+                else if (day == "Thursday")
+                {
+                    label_name.Text = male_name[3];
+                }
+                else if (day == "Friday")
+                {
+                    label_name.Text = male_name[4];
+                }
+                else if (day == "Saturday")
+                {
+                    label_name.Text = male_name[5];
+                }
+                else if (day == "Sunday")
+                {
+                    label_name.Text = male_name[6];
+                }
+            }
+            else
+                label_name.Text = "Error";
+                
+        }
+
+        
+    }
+}
